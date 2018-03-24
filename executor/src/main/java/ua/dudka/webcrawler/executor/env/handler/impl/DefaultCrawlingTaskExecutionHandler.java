b8f94167f84733a3e1de39346e7bc000ab183770
rostislav.dudka <rostislav.dudka@gmail.com>
2018-03-24 09:29:20 +0100

    - add logging to DefaultCrawlingTaskExecutionHandler

--- executor/src/main/java/ua/dudka/webcrawler/executor/env/handler/impl/DefaultCrawlingTaskExecutionHandler.java
+++ executor/src/main/java/ua/dudka/webcrawler/executor/env/handler/impl/DefaultCrawlingTaskExecutionHandler.java
@@ -1,21 +1,24 @@
 package ua.dudka.webcrawler.executor.env.handler.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Component;
-import ua.dudka.webcrawler.executor.env.handler.CrawlingTaskExecutionHandler;
-import ua.dudka.webcrawler.executor.env.stream.CrawlingTaskStreams;
 import ua.dudka.webcrawler.executor.domain.model.ExecuteCrawlingTaskEvent;
 import ua.dudka.webcrawler.executor.domain.service.CrawlingTaskExecutor;
+import ua.dudka.webcrawler.executor.env.handler.CrawlingTaskExecutionHandler;
+import ua.dudka.webcrawler.executor.env.stream.CrawlingTaskStreams;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DefaultCrawlingTaskExecutionHandler implements CrawlingTaskExecutionHandler {
     private final CrawlingTaskExecutor executor;
 
     @StreamListener(CrawlingTaskStreams.EXECUTE_TASK_INPUT_STREAM)
     @Override
     public void handle(ExecuteCrawlingTaskEvent task) {
+        log.info("Handling {}", task);
         executor.execute(task);
     }
 }
