63ce469f31cb27f497affa622e40ab7492fa0edc
rostislav.dudka <rostislav.dudka@gmail.com>
2018-04-07 05:53:41 +0200

    - make messaging gateway from event sender

--- client/src/main/java/ua/dudka/webcrawler/client/app/gateway/ExecuteCrawlingTaskEventSender.java
+++ client/src/main/java/ua/dudka/webcrawler/client/app/gateway/ExecuteCrawlingTaskEventSender.java
@@ -1,7 +1,14 @@
 package ua.dudka.webcrawler.client.app.gateway;
 
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
 import ua.dudka.webcrawler.client.app.gateway.event.ExecuteCrawlingTaskEvent;
 
+import static ua.dudka.webcrawler.client.app.config.CrawlingTaskStreams.EXECUTE_TASK_OUTPUT_STREAM;
+
+@MessagingGateway
 public interface ExecuteCrawlingTaskEventSender {
+
+    @Gateway(requestChannel = EXECUTE_TASK_OUTPUT_STREAM)
     void sendForExecution(ExecuteCrawlingTaskEvent event);
 }
