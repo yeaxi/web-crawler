21ccf73569f099eefc037471b3552d928da4df43
rostislav.dudka <rostislav.dudka@gmail.com>
2018-04-12 20:04:44 +0200

    - make DummyExecuteCrawlingTaskEventSender used only in dev profile

--- client/src/main/java/ua/dudka/webcrawler/client/app/gateway/impl/DummyExecuteCrawlingTaskEventSender.java
+++ client/src/main/java/ua/dudka/webcrawler/client/app/gateway/impl/DummyExecuteCrawlingTaskEventSender.java
@@ -1,11 +1,13 @@
 package ua.dudka.webcrawler.client.app.gateway.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import ua.dudka.webcrawler.client.app.gateway.ExecuteCrawlingTaskEventSender;
 import ua.dudka.webcrawler.client.app.gateway.event.ExecuteCrawlingTaskEvent;
 
 @Component
+@Profile("dev")
 @Slf4j
 public class DummyExecuteCrawlingTaskEventSender implements ExecuteCrawlingTaskEventSender {
 
