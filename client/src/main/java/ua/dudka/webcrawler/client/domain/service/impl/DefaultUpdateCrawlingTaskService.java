0d1c2d13a00a0feeb8557634c4d32c78605b5494
rostislav.dudka <rostislav.dudka@gmail.com>
2018-04-25 09:00:10 +0200

    - add check for nonexistent CrawlingTask in DefaultUpdateCrawlingTaskService;

--- client/src/main/java/ua/dudka/webcrawler/client/domain/service/impl/DefaultUpdateCrawlingTaskService.java
+++ client/src/main/java/ua/dudka/webcrawler/client/domain/service/impl/DefaultUpdateCrawlingTaskService.java
@@ -7,7 +7,9 @@ import reactor.core.publisher.Mono;
 import ua.dudka.webcrawler.client.app.gateway.ExecuteCrawlingTaskEventSender;
 import ua.dudka.webcrawler.client.app.gateway.event.ExecuteCrawlingTaskEvent;
 import ua.dudka.webcrawler.client.app.gateway.event.UpdateCrawlingTaskEvent;
+import ua.dudka.webcrawler.client.domain.model.CrawlingTask;
 import ua.dudka.webcrawler.client.domain.service.UpdateCrawlingTaskService;
+import ua.dudka.webcrawler.client.exception.TaskNotFoundException;
 import ua.dudka.webcrawler.client.repository.CrawlingTaskRepository;
 
 @Service
@@ -20,18 +22,33 @@ public class DefaultUpdateCrawlingTaskService implements UpdateCrawlingTaskServi
     @Override
     public Mono<Void> updateTask(UpdateCrawlingTaskEvent event) {
         return repository.findById(event.getTaskId())
-                .map(task -> {
-                    task.addVisitedLink(event.getVisitedLink());
-                    return task;
-                })
-                .doOnSuccess(repository::save)
+                .doOnSuccess(t -> checkForEmpty(t, event.getTaskId()))
+                .flatMap(task -> addVisitedLink(event, task))
                 .doOnSuccess(t -> sendForExecution(event))
                 .doOnError(throwable -> log.info(throwable.getMessage()))
                 .onErrorResume(throwable -> Mono.empty())
                 .then();
     }
 
+    private CrawlingTask checkForEmpty(CrawlingTask task, String id) {
+        if (task == null)
+            throw new TaskNotFoundException(id);
+
+        return task;
+    }
+
+    private Mono<CrawlingTask> addVisitedLink(UpdateCrawlingTaskEvent event, CrawlingTask task) {
+        log.info("adding link {} to the {}", event.getVisitedLink(), task);
+
+        task.addVisitedLink(event.getVisitedLink());
+        return repository.save(task);
+    }
+
     private void sendForExecution(UpdateCrawlingTaskEvent event) {
-        sender.sendForExecution(new ExecuteCrawlingTaskEvent(event.getTaskId(), event.getVisitedLink()));
+        ExecuteCrawlingTaskEvent executeEvent = new ExecuteCrawlingTaskEvent(event.getTaskId(), event.getVisitedLink());
+
+        log.info("sending {}", executeEvent);
+
+        sender.sendForExecution(executeEvent);
     }
 }
